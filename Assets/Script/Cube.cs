7d78693904d321da7fd5de3a600643b0758c5986
Pigeon-hcy <129189900+Pigeon-hcy@users.noreply.github.com>
2024-07-07 23:36:51 -0400

    change block

--- Assets/Script/Cube.cs
+++ Assets/Script/Cube.cs
@@ -2,6 +2,7 @@ using MoreMountains.Feedbacks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Cube : MonoBehaviour, IInteractable
 {
@@ -9,7 +10,10 @@ public class Cube : MonoBehaviour, IInteractable
     public GameManger GameManger;
     public AudioSource AudioSource;
     public MMF_Player OnHitFeedBack;
-
+    public int cooldown = 10;
+    public int cooldownRemaining;
+    public bool canIntercat;
+    public GameObject EParticle;
     private void Awake()
     {
         GameManger = GameObject.FindGameObjectWithTag("GameManger").GetComponent<GameManger>();
@@ -17,10 +21,42 @@ public class Cube : MonoBehaviour, IInteractable
 
     public void InteractWith()
     {
-        AudioSource.Play();
-        OnHitFeedBack.PlayFeedbacks();
-        GameManger.Emotion += 1;
+        
+            AudioSource.Play();
+            OnHitFeedBack.PlayFeedbacks();
+            
+            
+        if (canIntercat)
+        {
+            canIntercat = false;
+            cooldownRemaining = cooldown;
+            StartCoroutine(AddEmo());
+        }
+        
     }
 
-    
+    private void Update()
+    {
+        if (!canIntercat) 
+        {
+            cooldownRemaining--;
+        }
+
+        if (cooldownRemaining < 0 && !canIntercat) 
+        {
+            canIntercat = true;
+        }
+    }
+
+
+    IEnumerator AddEmo()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Instantiate(EParticle, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(1);
+        GameManger.Emotion += 1;
+    }
 }
